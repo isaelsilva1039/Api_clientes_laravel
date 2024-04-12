f1c845d551e05cefd2b7fa7b72c35e30513b7604
Isael <isaelsiva1039@gmail.com>
2024-04-12 15:15:45 -0300

    logs

--- app/Manager/ApiAsaasManager/ApiAsaasManager.php
+++ app/Manager/ApiAsaasManager/ApiAsaasManager.php
@@ -33,30 +33,28 @@ class ApiAsaasManager extends Controller
     }
 
 
-    /**
-     * @param Request $request
-     * @return array $data
-     */
     public function novoCliente(Request $request)
     {
         $billing = $request->input('billing', []);
         $status = $request->input('status');
         $lineItems = $request->input('line_items', []);
-        Log::error('request - > retorno : ' . $request);
-        Log::error('billing - > retorno : ' . $billing);
 
-        $asaas = new Asaas(ApiAsaasManager::CHAVE_API_ASSAS);
+        // Logar os dados de entrada para fins de depuração (assegure-se de não logar dados sensíveis em produção)
+        Log::info('Request billing data: ' . json_encode($billing));
+        Log::info('Request line items: ' . json_encode($lineItems));
+
+        $asaas = new Asaas(self::CHAVE_API_ASSAS);
 
         try {
             $productName = $lineItems[0]['name'] ?? 'Produto não especificado';
             $observations = "Cliente importado do sistema X - Plano: {$productName}";
 
             $dados = [
-                "name" => ($billing['first_name'] ?? 'Usuário não informou o nome -') . ' ' . ($billing['last_name'] ?? ''),
+                "name" => ($billing['first_name'] ?? 'Nome não informado') . ' ' . ($billing['last_name'] ?? ''),
                 "email" => $billing['email'] ?? 'email@padrao.com',
                 "phone" => $billing['phone'] ?? '',
                 "mobilePhone" => $billing['cellphone'] ?? '',
-                "cpfCnpj" => $billing['cpf'] ?? ($billing['cnpj'] ?? '07761854386'),
+                "cpfCnpj" => $billing['cpf'] ?? ($billing['cnpj'] ?? 'CPF/CNPJ não informado'),
                 "postalCode" => $billing['postcode'] ?? '',
                 "address" => $billing['address_1'] ?? '',
                 "addressNumber" => $billing['number'] ?? '',
@@ -69,9 +67,7 @@ class ApiAsaasManager extends Controller
 
             $data = $asaas->Cliente()->create($dados);
             if ($data) {
-                Log::error('Entrou para adicionar no banco: ' . $data);
                 $data = $this->apiManagerCadastro->inserirNoBanco($dados);
-                Log::error('Entrou para adicionar no banco - > retorno : ' . $data);
             }
         } catch (\Throwable $e) {
             Log::error('Erro ao criar cliente: ' . $e->getMessage());
