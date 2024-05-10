6c3ae4879db722976d0001b80f53e2c0b16a4aa9
Isael <isaelsiva1039@gmail.com>
2024-05-10 18:11:58 -0300

    ajustes

--- index.php
+++ index.php
@@ -1,55 +1,19 @@
 <?php
 
-use Illuminate\Contracts\Http\Kernel;
-use Illuminate\Http\Request;
+/**
+ * Redireciona todas as solicitações para o diretório 'public'
+ */
 
-define('LARAVEL_START', microtime(true));
+$public = 'public/';
 
-/*
-|--------------------------------------------------------------------------
-| Check If The Application Is Under Maintenance
-|--------------------------------------------------------------------------
-|
-| If the application is in maintenance / demo mode via the "down" command
-| we will load this file so that any pre-rendered content can be shown
-| instead of starting the framework, which could cause an exception.
-|
-*/
+$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+$uri = '/'. ltrim($uri, '/');
 
-if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
-    require $maintenance;
+$target = $public . $uri;
+if(file_exists(__DIR__ . $target)) {
+    // Se o arquivo existir em 'public', sirva esse arquivo diretamente
+    return require_once __DIR__ . $target;
 }
 
-/*
-|--------------------------------------------------------------------------
-| Register The Auto Loader
-|--------------------------------------------------------------------------
-|
-| Composer provides a convenient, automatically generated class loader for
-| this application. We just need to utilize it! We'll simply require it
-| into the script here so we don't need to manually load our classes.
-|
-*/
-
-require __DIR__.'/../vendor/autoload.php';
-
-/*
-|--------------------------------------------------------------------------
-| Run The Application
-|--------------------------------------------------------------------------
-|
-| Once we have the application, we can handle the incoming request using
-| the application's HTTP kernel. Then, we will send the response back
-| to this client's browser, allowing them to enjoy our application.
-|
-*/
-
-$app = require_once __DIR__.'/../bootstrap/app.php';
-
-$kernel = $app->make(Kernel::class);
-
-$response = $kernel->handle(
-    $request = Request::capture()
-)->send();
-
-$kernel->terminate($request, $response);
+// Se não, redirecione para 'public/index.php'
+require_once __DIR__ . '/public/index.php';
